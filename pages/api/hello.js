eed52d2bdcdde51c33ce6ae044ea500060d96c89
SahTitus <sahtitus58@gmail.com>
2023-01-03 13:17:39 +0000

    updates

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default async function handler(req, res) {
-  res.status(200).json({ name: 'SAM Doe' })
+export default function handler(req, res) {
+  res.status(200).json({ name: 'Sam nie' })
 }
