f98ec7f26cfffbbf848cfd205ff50ce67c2492d8
SahTitus <sahtitus58@gmail.com>
2023-01-03 12:48:26 +0000

    updates

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,134 +1,132 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default async function handler(req, res) {
+export default function handler(req, res) {
   res.status(200).json( {
-    statusCode: 200,
-    body: JSON.stringify({
-      articles: [
-        {
-          "_id": "63a78085e83b5ef9fcdee964",
-          "link": "https://www.livestrong.com/article/13767676-black-toenail-treatment/",
-          "title": "What to Do About a Black Toenail, According to a Podiatrist",
-          "image": "https://img.livestrong.com/375/cme-data/getty%2Fcc679d8e4441451ca3e845e1f243ab3c.jpg",
-          "source": "Livestrong",
-          "category": "joint ligament tendon-conditions",
-          "source_img": "https://logodix.com/logo/1739400.png",
-          "category_id": "joint-ligament-tendon-conditions",
-          "__v": 0,
-          "saves": []
-        },
-        {
-          "_id": "63a7818fe83b5ef9fcdefaf9",
-          "link": "https://www.healthline.com/human-body-maps/superficial-peroneal-nerve",
-          "title": "Superficial peroneal nerve",
-          "image": "https://i0.wp.com/post.healthline.com/wp-content/uploads/2020/07/nervous3-4.jpg?w=176",
-          "source": "Healthline",
-          "category": "human body maps",
-          "mini_card": true,
-          "source_img": "https://cdn.comparably.com/26017579/l/128313_logo_healthline-media.png",
-          "category_id": "human-body-maps",
-          "__v": 0,
-          "saves": []
-        },
-        {
-          "_id": "63a781a8e83b5ef9fcdefe74",
-          "link": "https://www.onhealth.com/content/1/feel_awake_10_ways_without_caffeine",
-          "title": "10 Ways to Feel Awake Without Caffeine",
-          "image": "https://images.onhealth.com/images/slideshow/xl-sq-promos/10-ways-to-feel-awake-without-caffeine.jpg",
-          "source": "Onhealth",
-          "category": "wellness",
-          "source_img": "https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/v1490183531/w91fuklgfkc0urqejhsg.png",
-          "category_id": "wellness",
-          "__v": 0,
-          "saves": []
-        },
-        {
-          "_id": "63a781bee83b5ef9fcdf040b",
-          "link": "https://www.shape.com/health/sexual-health/can-birth-control-cause-hair-loss",
-          "title": "The Relationship Between Birth Control and Hair Loss",
-          "image": "https://www.shape.com/thmb/iKhPH22uHrAnZSwev__JmMdG8YU=/282x188/filters:no_upscale():max_bytes(150000):strip_icc():focal(999x0:1001x2)/Can-Birth-Control-Cause-Hair-Loss-GettyImages-1339104935-1174855113-2000-6d2d0f9d256f4ef98832d42a66556a85.jpg",
-          "source": "Shape",
-          "category": "sexual health ",
-          "mini_card": true,
-          "source_img": "https://upload.wikimedia.org/wikipedia/commons/a/a2/Shape_magazine_logo.jpg",
-          "category_id": "sexual-health ",
-          "__v": 0,
-          "saves": []
-        },
-        {
-          "_id": "63a781a8e83b5ef9fcdefd04",
-          "link": "https://www.onhealth.com/content/1/how_can_parents_deal_with_temper_tantrums",
-          "title": "How Can Parents Deal With Temper Tantrums?",
-          "image": "https://images.onhealth.com/images/slideshow/xl-sq-promos/how-can-parents-deal-with-temper-tantrums.jpg",
-          "source": "Onhealth",
-          "category": "parenting",
-          "source_img": "https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/v1490183531/w91fuklgfkc0urqejhsg.png",
-          "category_id": "parenting",
-          "__v": 0,
-          "saves": []
-        },
-        {
-          "_id": "63a7801fe83b5ef9fcdee252",
-          "link": "https://www.webmd.com/baby/ss/slideshow-prepregnancy-checklist",
-          "title": " Your Prepregnancy Checklist ",
-          "image": "https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/couple_cooking_other/1800x1200_couple_cooking_other.jpg?resize=600px:*&output-quality=50",
-          "source": "Webmd",
-          "category": "parenting",
-          "source_img": "https://upload.wikimedia.org/wikipedia/commons/f/fb/WikiHow_initials.png?20160111212427",
-          "category_id": "parenting",
-          "__v": 0,
-          "saves": []
-        },
-        {
-          "_id": "63a78057e83b5ef9fcdee532",
-          "link": "https://justhealthlife.com/is-your-favorite-beverage-killing-you-slowly/",
-          "title": " Is Your Favorite Beverage Killing You Slowly?",
-          "image": "https://justhealthlife.com/wp-content/uploads/2022/06/beverages-glasses-350x250.jpg",
-          "source": "Justhealthlife",
-          "category": "nutrition",
-          "source_img": "https://justhealthlife.com/wp-content/uploads/2022/06/logo.svg",
-          "category_id": "nutrition",
-          "__v": 0,
-          "saves": []
-        },
-        {
-          "_id": "63a78132e83b5ef9fcdef47f",
-          "link": "https://www.medicalnewstoday.com/articles/321518",
-          "title": "Foods for gaining weight quickly and safelyPeople can gain weight by increasing their calorie intake. Learn about a variety of foods…",
-          "image": "https://i0.wp.com/post.medicalnewstoday.com/wp-content/uploads/sites/3/2022/08/gaining_weight_GettyImages769723307_Thumb-732x549.jpg?w=304",
-          "source": "MedicalNewsToday",
-          "category": "fitness",
-          "mini_card": true,
-          "source_img": "https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/v1413780870/nu5ilpby3btq45pghneg.png",
-          "category_id": "fitness",
-          "__v": 0,
-          "saves": []
-        },
-        {
-          "_id": "63a781b7e83b5ef9fcdf031f",
-          "link": "https://www.health.harvard.edu/mens-health/managing-premature-ejaculation",
-          "title": "Managing premature ejaculation",
-          "image": "https://domf5oio6qrcr.cloudfront.net/medialibrary/12739/81e8f5b6-2dda-4ae9-8f5b-7f67c56deac4.jpg",
-          "source": "Harvard",
-          "category": "men's health",
-          "source_img": "https://www.health.harvard.edu/img/logos/hhp_logo@2x.png",
-          "category_id": "mens-health",
-          "__v": 0,
-          "saves": []
-        },
-        {
-          "_id": "63a781a8e83b5ef9fcdeff26",
-          "link": "https://www.onhealth.com/content/1/locker_room_diseases",
-          "title": "Teen Boys: Sports, Gym and Locker Room Gross-Outs",
-          "image": "https://images.onhealth.com/images/slideshow/xl-sq-promos/locker-room-diseases.jpg",
-          "source": "Onhealth",
-          "category": "wellness",
-          "source_img": "https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/v1490183531/w91fuklgfkc0urqejhsg.png",
-          "category_id": "wellness",
-          "__v": 0,
-          "saves": []
-        }
-      ],
-    })
-  })
+  "articles": [
+    {
+      "_id": "63a78085e83b5ef9fcdee964",
+      "link": "https://www.livestrong.com/article/13767676-black-toenail-treatment/",
+      "title": "What to Do About a Black Toenail, According to a Podiatrist",
+      "image": "https://img.livestrong.com/375/cme-data/getty%2Fcc679d8e4441451ca3e845e1f243ab3c.jpg",
+      "source": "Livestrong",
+      "category": "joint ligament tendon-conditions",
+      "source_img": "https://logodix.com/logo/1739400.png",
+      "category_id": "joint-ligament-tendon-conditions",
+      "__v": 0,
+      "saves": []
+    },
+    {
+      "_id": "63a7818fe83b5ef9fcdefaf9",
+      "link": "https://www.healthline.com/human-body-maps/superficial-peroneal-nerve",
+      "title": "Superficial peroneal nerve",
+      "image": "https://i0.wp.com/post.healthline.com/wp-content/uploads/2020/07/nervous3-4.jpg?w=176",
+      "source": "Healthline",
+      "category": "human body maps",
+      "mini_card": true,
+      "source_img": "https://cdn.comparably.com/26017579/l/128313_logo_healthline-media.png",
+      "category_id": "human-body-maps",
+      "__v": 0,
+      "saves": []
+    },
+    {
+      "_id": "63a781a8e83b5ef9fcdefe74",
+      "link": "https://www.onhealth.com/content/1/feel_awake_10_ways_without_caffeine",
+      "title": "10 Ways to Feel Awake Without Caffeine",
+      "image": "https://images.onhealth.com/images/slideshow/xl-sq-promos/10-ways-to-feel-awake-without-caffeine.jpg",
+      "source": "Onhealth",
+      "category": "wellness",
+      "source_img": "https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/v1490183531/w91fuklgfkc0urqejhsg.png",
+      "category_id": "wellness",
+      "__v": 0,
+      "saves": []
+    },
+    {
+      "_id": "63a781bee83b5ef9fcdf040b",
+      "link": "https://www.shape.com/health/sexual-health/can-birth-control-cause-hair-loss",
+      "title": "The Relationship Between Birth Control and Hair Loss",
+      "image": "https://www.shape.com/thmb/iKhPH22uHrAnZSwev__JmMdG8YU=/282x188/filters:no_upscale():max_bytes(150000):strip_icc():focal(999x0:1001x2)/Can-Birth-Control-Cause-Hair-Loss-GettyImages-1339104935-1174855113-2000-6d2d0f9d256f4ef98832d42a66556a85.jpg",
+      "source": "Shape",
+      "category": "sexual health ",
+      "mini_card": true,
+      "source_img": "https://upload.wikimedia.org/wikipedia/commons/a/a2/Shape_magazine_logo.jpg",
+      "category_id": "sexual-health ",
+      "__v": 0,
+      "saves": []
+    },
+    {
+      "_id": "63a781a8e83b5ef9fcdefd04",
+      "link": "https://www.onhealth.com/content/1/how_can_parents_deal_with_temper_tantrums",
+      "title": "How Can Parents Deal With Temper Tantrums?",
+      "image": "https://images.onhealth.com/images/slideshow/xl-sq-promos/how-can-parents-deal-with-temper-tantrums.jpg",
+      "source": "Onhealth",
+      "category": "parenting",
+      "source_img": "https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/v1490183531/w91fuklgfkc0urqejhsg.png",
+      "category_id": "parenting",
+      "__v": 0,
+      "saves": []
+    },
+    {
+      "_id": "63a7801fe83b5ef9fcdee252",
+      "link": "https://www.webmd.com/baby/ss/slideshow-prepregnancy-checklist",
+      "title": " Your Prepregnancy Checklist ",
+      "image": "https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/couple_cooking_other/1800x1200_couple_cooking_other.jpg?resize=600px:*&output-quality=50",
+      "source": "Webmd",
+      "category": "parenting",
+      "source_img": "https://upload.wikimedia.org/wikipedia/commons/f/fb/WikiHow_initials.png?20160111212427",
+      "category_id": "parenting",
+      "__v": 0,
+      "saves": []
+    },
+    {
+      "_id": "63a78057e83b5ef9fcdee532",
+      "link": "https://justhealthlife.com/is-your-favorite-beverage-killing-you-slowly/",
+      "title": " Is Your Favorite Beverage Killing You Slowly?",
+      "image": "https://justhealthlife.com/wp-content/uploads/2022/06/beverages-glasses-350x250.jpg",
+      "source": "Justhealthlife",
+      "category": "nutrition",
+      "source_img": "https://justhealthlife.com/wp-content/uploads/2022/06/logo.svg",
+      "category_id": "nutrition",
+      "__v": 0,
+      "saves": []
+    },
+    {
+      "_id": "63a78132e83b5ef9fcdef47f",
+      "link": "https://www.medicalnewstoday.com/articles/321518",
+      "title": "Foods for gaining weight quickly and safelyPeople can gain weight by increasing their calorie intake. Learn about a variety of foods…",
+      "image": "https://i0.wp.com/post.medicalnewstoday.com/wp-content/uploads/sites/3/2022/08/gaining_weight_GettyImages769723307_Thumb-732x549.jpg?w=304",
+      "source": "MedicalNewsToday",
+      "category": "fitness",
+      "mini_card": true,
+      "source_img": "https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/v1413780870/nu5ilpby3btq45pghneg.png",
+      "category_id": "fitness",
+      "__v": 0,
+      "saves": []
+    },
+    {
+      "_id": "63a781b7e83b5ef9fcdf031f",
+      "link": "https://www.health.harvard.edu/mens-health/managing-premature-ejaculation",
+      "title": "Managing premature ejaculation",
+      "image": "https://domf5oio6qrcr.cloudfront.net/medialibrary/12739/81e8f5b6-2dda-4ae9-8f5b-7f67c56deac4.jpg",
+      "source": "Harvard",
+      "category": "men's health",
+      "source_img": "https://www.health.harvard.edu/img/logos/hhp_logo@2x.png",
+      "category_id": "mens-health",
+      "__v": 0,
+      "saves": []
+    },
+    {
+      "_id": "63a781a8e83b5ef9fcdeff26",
+      "link": "https://www.onhealth.com/content/1/locker_room_diseases",
+      "title": "Teen Boys: Sports, Gym and Locker Room Gross-Outs",
+      "image": "https://images.onhealth.com/images/slideshow/xl-sq-promos/locker-room-diseases.jpg",
+      "source": "Onhealth",
+      "category": "wellness",
+      "source_img": "https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/v1490183531/w91fuklgfkc0urqejhsg.png",
+      "category_id": "wellness",
+      "__v": 0,
+      "saves": []
+    }
+  ],
+  "totalCount": 5020
+})
 }
